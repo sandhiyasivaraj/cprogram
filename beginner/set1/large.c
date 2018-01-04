8b13f0ea8299987aff393281c581d6bd05e46ac1
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-01-04 07:25:44 +0530

    Update large.c

--- beginner/set1/large.c
+++ beginner/set1/large.c
@@ -3,6 +3,6 @@ int main(void)
 {
 int a,b,c;
 scanf("%d%d%d",&a,&b,&c);
-a>b&&a>c?printf("%d",a):b>c&&b>a?printf("%d",b):printf("%d",c);
+a>=b&&a>=c?printf("%d",a):b>=c&&b>=a?printf("%d",b):printf("%d",c);
 return 0;
 }
