5741fe0fcc3612b4a773315a12dfc425db662d99
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-01-04 18:59:04 +0530

    Update 16.prinum.c

--- beginner/set2/16.prinum.c
+++ beginner/set2/16.prinum.c
@@ -5,18 +5,20 @@ int n1,n2,i,j,flag=0;
 scanf("%d%d",&n1,&n2);
 for(i=n1;i<n2;i++) 
 { 
-	flag=0;
+	{
+flag=0;	
 	for(j=2;j<i;j++)
 	{
-		
 		if(i%j==0) 
-		flag++;
+		flag=1;
+		}
+	if(i==1)
+	{
+		continue;
 	}
- 
-
-if(flag==0)
-printf("%d ",i);
+	}
+ if(flag==0)
+		printf("%d ",i);
 }
-
 return 0;
 }
