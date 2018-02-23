8f2571befc0e4630fb77573b80b582452e55cc71
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-02-23 11:47:12 +0530

    Create 113.repeating_count.c

---
+++ beginner/set5/113.repeating_count.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int N,K,i,a[N],count=0;
+	scanf("%d %d",&N,&K);
+	for(i=0;i<N;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<N;i++)
+	{
+	if(a[i]==K)
+	{
+		count++;
+		
+	}
+	}
+	if(count>0)
+	printf("%d",count);
+
+	return 0;
+}
