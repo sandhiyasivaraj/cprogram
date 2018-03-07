0a269c6ab1b43a18e1abec4c798706a443228c03
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-03-07 12:35:24 +0530

    Create 41.string.c

---
+++ beginner/set5.1/41.string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char S[100];
+	int i,N;
+	scanf("%s",S);
+	scanf("%d",&N);
+	for(i=0;i<N;i++)
+	{
+	printf("%s",S);
+	printf("\n");
+	}
+	return 0;
+}
