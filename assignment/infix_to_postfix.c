f23c300c71261033a24942082de713f41737ccab
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-03-09 07:27:47 +0530

    Create infix_to_postfix.c

---
+++ assignment/infix_to_postfix.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+	{
+	   char data;
+           struct node *next;
+	};
+typedef struct node n;
+n *top;
+char output[15];
+int j=0;
+
+
+//precedence function
+int getprecedence(char ch)
+	{
+	  int value;
+	  switch(ch)
+		{
+		    case '+':value=1;break;
+		    case '-':value=1;break;
+		    case '*':value=2;break;
+		    case '/':value=2;break;
+		    default :value=5;break;
+		}
+        }
+
+//puch function
+void push(char ch)
+	{
+	   n *newnode;
+	   newnode=(n*)malloc(sizeof(n));
+	   newnode->data=ch;
+	   newnode->next=top;
+	   top=newnode;
+	}
+
+//pop function
+char pop()
+	{
+	    n *temp=top;
+	    if(top!=NULL)
+	    top=top->next;
+	    return(top->data);
+	}
+
+
+int main(void)
+	{
+	   int i,j=0;
+	   char input[15],k;
+	   printf("ENTER THE INFIX EXPRESSION");
+	   scanf("%s",input);
+for(i=0;i<strlen(input);i++)
+	{
+	  if(i!='+'&&i!='-'&&i!='*'&&i!='/')
+	     {
+		output[j]=input[i];
+		j++;
+	     }
+	  else
+	     {
+		k=pop();
+		if(getprecedence(k)==5)
+		   {
+		      push(input[i]);
+		   }
+
+		else if(getprecedence(k)<getprecedence(input[i]))
+		  {
+		     push(k);
+		     push(input[i]);
+		  }
+		else
+		  {
+		     output[j]=k;
+		     j++;
+		     push(input[i]);
+		  }
+	     }
+	}
+
+while(k=pop())
+	{
+	  output[j]=k;
+	  j++;
+	}
+ printf("%s",output);
+return 0;
+}
