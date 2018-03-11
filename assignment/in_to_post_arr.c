e33f40854b600369b0c00312fe8aec022b38d2f9
sandhiyasivaraj <34916422+sandhiyasivaraj@users.noreply.github.com>
2018-03-11 14:11:32 +0530

    Update in_to_post_arr.c

--- assignment/in_to_post_arr.c
+++ assignment/in_to_post_arr.c
@@ -1,53 +1,61 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 char output[15];
 int j=0;char stack[10];
 int size=10;
 int top=-1;
+
+
+//precedence function
 int getprecedence(char ch)
-{
-int value;
-switch(ch)
-{
-case '+':value=1;break;
-case '-':value=1;break;
-case '*':value=2;break;
-case '/':value=2;break;
-default:value=5;break;
-}
-return value;
-}
+	{
+	  int value;
+	  switch(ch)
+		{
+		    case '+':value=1;break;
+		    case '-':value=1;break;
+		    case '*':value=2;break;
+		    case '/':value=2;break;
+		    default :value=5;break;
+		}
+	return value;
+        }
+
+//puch function
 void push(char ch)
 {
+
 top++;
 stack[top]=ch;
+
 }
+//pop function
 char pop()
 {
 return(stack[top]);
 top--;
 
-
 }
-int main(void)
-{
-int i,l;
-char k;
-char input[15];
 
-printf("ENTER THE INFIX EXPRESSION\n");
-scanf("%s",input);
-l=strlen(input);
-for(i=0;i<l;i++)
-{
-if(input[i]!='+'&&input[i]!='-'&&input[i]!='*'&&input[i]!='/')
+
+int main(void)
+	{
+	   int i,l;
+	   char input[15],k;
+	   printf("ENTER THE INFIX EXPRESSION\n");
+	   scanf("%s",input);
+	   l=strlen(input);
+for(i=0;i<strlen(input);i++)
+	{
+	  if(input[i]!='+'&&input[i]!='-'&&input[i]!='*'&&input[i]!='/')
 	     {
 		output[j]=input[i];
 		j++;
-		
 	     }
-else
+	  else
 	     {
+		
 		k=pop();
 		if(getprecedence(k)==5)
 		   {
@@ -67,13 +75,15 @@ else
 		  }
 	     }
 	}
-while(1)
+int l1=strlen(output);
+for(i=1;i<=l-l1;i++)
 {
-k=pop();
-output[j]=k;
-j++;
+if(k=pop())
+	{
+	  output[j]=k;
+	  j++;
+	}
+top--;
 }
-printf("%s",output);
-return 0;
+ printf("%s",output);
 }
-
